b8f18822cfd73898add76d09b39d7e17c7327046
sgmad <155355629+sgmad@users.noreply.github.com>
2026-03-04 11:03:57 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,21 @@
+<?php
+include 'db.php';
+
+if (isset($_POST['submit'])) {
+    $student_id = $_POST['student_id'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $course_name = $_POST['course_name'];
+    $enrollment_date = $_POST['enrollment_date'];
+
+    $sql1 = "UPDATE student SET FirstName='$first_name', LastName='$last_name', Email='$email' WHERE StudentId='$student_id'";
+    mysqli_query($conn, $sql1);
+
+    $sql2 = "UPDATE enrollment SET CourseName='$course_name', EnrollmentDate='$enrollment_date' WHERE StudentId='$student_id'";
+    mysqli_query($conn, $sql2);
+
+    header("Location: index.php");
+}
+?>
 
