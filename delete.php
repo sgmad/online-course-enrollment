9b83ede10766dea4c688a47edeeb2577444488d7
sgmad <155355629+sgmad@users.noreply.github.com>
2026-03-04 11:03:35 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,13 @@
+<?php
+include 'db.php';
+
+if (isset($_GET['id'])) {
+    $student_id = $_GET['id'];
+    
+    $sql = "DELETE FROM student WHERE StudentId = '$student_id'";
+    mysqli_query($conn, $sql);
+
+    header("Location: index.php");
+}
+?>
 
