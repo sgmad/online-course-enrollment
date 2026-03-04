8d508e36db5aabda9e12542c738b251df71fe4f5
sgmad <155355629+sgmad@users.noreply.github.com>
2026-03-04 11:04:14 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,20 +1,21 @@
-
 <?php
-require 'db.php';
+include 'db.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $stmt1 = $conn->prepare("INSERT INTO student (FirstName, LastName, Email) VALUES (?, ?, ?)");
-    $stmt1->bind_param("sss", $_POST['first_name'], $_POST['last_name'], $_POST['email']);
-    $stmt1->execute();
-    $student_id = $conn->insert_id;
-    $stmt1->close();
+if (isset($_POST['submit'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $course_name = $_POST['course_name'];
+    $enrollment_date = $_POST['enrollment_date'];
 
-    $stmt2 = $conn->prepare("INSERT INTO enrollment (StudentId, CourseName, EnrollmentDate) VALUES (?, ?, ?)");
-    $stmt2->bind_param("iss", $student_id, $_POST['course_name'], $_POST['enrollment_date']);
-    $stmt2->execute();
-    $stmt2->close();
+    $sql1 = "INSERT INTO student (FirstName, LastName, Email) VALUES ('$first_name', '$last_name', '$email')";
+    mysqli_query($conn, $sql1);
     
+    $student_id = mysqli_insert_id($conn);
+
+    $sql2 = "INSERT INTO enrollment (StudentId, CourseName, EnrollmentDate) VALUES ('$student_id', '$course_name', '$enrollment_date')";
+    mysqli_query($conn, $sql2);
+
     header("Location: index.php");
-    exit();
 }
 ?>
